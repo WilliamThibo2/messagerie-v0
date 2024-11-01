f3ea66bab26e2baec3c5b63eb483a08470aee569
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-01 19:43:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-    },
-});
+const io = new Server(server);
 
+// Servir les fichiers statiques du dossier public
+app.use(express.static(path.join(__dirname, "public")));
+
+// Point d'entrée principal
 app.get("/", (req, res) => {
-    res.send("Serveur de chat en temps réel");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Configuration de Socket.io
 io.on("connection", (socket) => {
-    console.log("Nouvel utilisateur connecté");
+    console.log("Un utilisateur est connecté");
 
-    // Réception et diffusion des messages
-    socket.on("chatMessage", (msg) => {
-        io.emit("message", msg);
+    socket.on("message", (msg) => {
+        io.emit("message", msg); // Diffuse le message à tous les utilisateurs connectés
     });
 
     socket.on("disconnect", () => {
-        console.log("Utilisateur déconnecté");
+        console.log("Un utilisateur est déconnecté");
     });
 });
 
+// Démarrer le serveur
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
+server.listen(PORT, () => {
+    console.log(`Serveur démarré sur le port ${PORT}`);
+});
