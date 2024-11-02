c350106169d98b8ac6d21b99eb1d2522569e143a
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-02 09:31:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -84,10 +84,5 @@ io.use((socket, next) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Serveur démarré sur le port ${PORT}`);
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
