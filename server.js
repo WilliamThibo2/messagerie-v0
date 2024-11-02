85096d63810c55f96c0072a441123ad70316bcfa
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-02 10:41:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -87,3 +87,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
+
+server.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
