0fa4709d9195dfb680e1d07eb6d51b6efdd2cf45
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-01 20:36:48 -0400

    Auth process added

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public"))); // Sert le dossier public
 
+// Route principale
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
@@ -16,12 +17,22 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
     console.log("Un utilisateur est connecté");
 
+    // Gestion de l'authentification avec username
+    socket.on("join", (username) => {
+        socket.username = username; // Associe un pseudonyme à la connexion
+        io.emit("message", `${username} a rejoint le chat`);
+    });
+
     socket.on("message", (msg) => {
-        io.emit("message", msg);
+        // Émet le message avec le pseudonyme de l'utilisateur
+        io.emit("message", `${socket.username}: ${msg}`);
     });
 
     socket.on("disconnect", () => {
         console.log("Un utilisateur s'est déconnecté");
+        if (socket.username) {
+            io.emit("message", `${socket.username} a quitté le chat`);
+        }
     });
 });
 
